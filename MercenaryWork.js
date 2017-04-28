ab1071677f9ea4624c77fb86d61dc7edde4ceba3
Jeremyb498 <Jeremyrox498@gmail.com>
2017-04-28 13:55:03 -0400

    Add files via upload

---
+++ MercenaryWork.js
@@ -0,0 +1,93 @@
+/*var map = [
+	[1,1,1,1,1]
+	[1,0,0,0,1]
+	[1,0,0,0,1]
+	[1,0,0,0,1]
+	[1,1,1,1,1]
+	]*/
+(function(){
+	
+	var canvas = document.getElementById("canvas");   // the canvas where game will be drawn
+	var context = canvas.getContext("2d");            // canvas context
+	var levelCols=40;	// level width, in tiles
+	var levelRows=20;	// level height, in tiles
+	var tileSize=16;	// tile size, in pixels
+	var playerCol=20;                                  // player starting column
+	var playerRow=10;                                  // player starting row
+	
+	var level = [        	// the level - 1=wall, 0=empty space
+	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+	];
+ 
+	var playerYPos=playerRow*tileSize;	// converting Y player position from tiles to pixels
+	var playerXPos=playerCol*=tileSize;               // converting X player position from tiles to pixels
+	
+	canvas.width=tileSize*levelCols;                   // canvas width. Won't work without it even if you style it from CSS
+	canvas.height=tileSize*levelRows;                   // canvas height. Same as before
+	
+	/*var Dungeon = new Image();
+	Dungeon.src = "Assets/Terrain/dungeon_tiles.png";
+	var DungeonFloor = new Image();
+	DungeonFloor.src = "Assets/Terrain/BackgroundDun2.png";
+	var DungeonFloor1 = new Image();
+	DungeonFloor1.src = "Assets/Terrain/BackgroundDun.png";
+*/
+	renderLevel();
+	
+	// function to display the level
+	
+	function renderLevel(){
+	// clear the canvas
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	// walls = red boxes
+	/*DungeonFloor.onload = function() {
+		for(i=0;i<levelRows;i++){
+		for(j=0;j<levelCols;j++){
+		if(level[i][j]==1){
+		context.drawImage(DungeonFloor,j,i,tileSize,tileSize);
+		}
+			DungeonFloor1.onload = function() {
+		for(i=0;i<levelRows;i++){
+		for(j=0;j<levelCols;j++){
+		if(level[i][j]==0){
+		context.drawImage(DungeonFloor1,j,i,tileSize,tileSize);
+		}*/
+	
+	
+
+	// player = green box
+	context.fillStyle = "#00ff00";
+	context.fillRect(playerXPos,playerYPos,tileSize,tileSize);
+	context.fillStyle = "#ff0000";
+	for(i=0;i<levelRows;i++){
+	for(j=0;j<levelCols;j++){
+	if(level[i][j]==1){
+	context.fillRect(j*tileSize,i*tileSize,tileSize,tileSize);	
+	}
+	}
+	}
+	// player = green box
+	context.fillStyle = "#00ff00";
+	context.fillRect(playerXPos,playerYPos,tileSize,tileSize);
+	}
+ 
+})();
