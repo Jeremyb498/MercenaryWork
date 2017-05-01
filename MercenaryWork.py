69ee297f99e5c1ab4314461958747b61afaf0e5a
Jacob Knox <jacobknox@dixie.k12.fl.us>
2017-05-01 08:38:49 -0400

    Add files via upload
    Possible sound/music for the game (both the script and in mp3 format)

---
+++ MercenaryWork.py
@@ -0,0 +1,16 @@
+#		python code
+#		script_name:
+#
+#		author:
+#		description:
+#
+
+from earsketch import *
+
+init()
+setTempo(120)
+
+fitMedia(DUBSTEP_LEAD_015,1,1,5)
+fitMedia(DUBSTEP_FILTERCHORD_001,2,1,5)
+
+finish()
